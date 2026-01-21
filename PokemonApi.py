a70193344399f6501185ca4aa976c2c61dd2fe56
Şahin Avcı <132500203+SahinAvc@users.noreply.github.com>
2026-01-21 23:01:48 +0300

    added pokemonapp
    Added a GUI for Pokemon lookup using Tkinter.

--- PokemonApi.py
+++ PokemonApi.py
@@ -1,4 +1,5 @@
 import requests
+import tkinter as tk
 
 def get_pokemon(name):
     url = f"https://pokeapi.co/api/v2/pokemon/{name.lower()}"
@@ -9,21 +10,62 @@ def get_pokemon(name):
 
         pokemon_name = data["name"].capitalize()
         pokemon_id = data['id']
-        height = data['height']
-        weight = data['weight']
+        height = data['height'] / 10   
+        weight = data['weight'] / 10   
 
-        print(f"Pokemon: {pokemon_name} (ID: {pokemon_id})")
-        print(f"Height: {height/10} m")
-        print(f"Weight: {weight/10} kg")
-
-        print("Stats:")
+        stats_text = ""
         for stat in data['stats']:
             stat_name = stat["stat"]["name"]
             stat_value = stat["base_stat"]
-            print(f"\t{stat_name}: {stat_value}")
+            stats_text += f"{stat_name}: {stat_value}\n"
 
+        return f"Pokemon: {pokemon_name} (ID: {pokemon_id})\nHeight: {height} m\nWeight: {weight} kg\n\nStats:\n{stats_text}"
     else:
-        print("No pokemon found.")
+        return "No pokemon found."
 
-if __name__ == "__main__":
-    get_pokemon("pikachu")
+def open_new_screen(nick,result):
+    new_screen= tk.Toplevel(screen)
+    new_screen.title(f"Pokemon {nick}")
+    new_screen.geometry("300x400")
+
+    infoLabel = tk.Label(new_screen,text=result,font=('Arial',16,"bold"))
+    infoLabel.pack(pady=20)
+
+def click_button():
+    nick = entryUser.get()
+    result = get_pokemon(nick)
+    if get_pokemon(nick) == "No pokemon found.":
+        resultLabel.config(text="unsuccessful",fg="red")
+        
+    else:
+        resultLabel.config(text="Succes",fg="green")
+        open_new_screen(nick,result)
+        
+#window
+screen = tk.Tk()
+screen.title("Pokemon App")
+screen.minsize(height=600,width=800)
+screen.configure(bg="lightblue")
+
+#label
+pokemon = tk.Label(
+    text="Enter Pokemon:",
+    font=('Arial', 15, "normal"),
+    bg="lightblue",
+    fg="darkblue"
+)
+pokemon.place(x=200, y=250)
+
+
+#entry
+entryUser = tk.Entry(width=20)
+entryUser.place(x=350,y=257)
+
+#button
+buttonUser = tk.Button(text="Ok",width=5,height=1,command=click_button,bg="lightblue",fg="white").place(x=500,y=253)
+
+#resultLabel
+resultLabel = tk.Label(text="",font=('Arial',12,"bold"),bg="lightblue")
+resultLabel.place(x=300,y=500)
+
+screen.mainloop()
